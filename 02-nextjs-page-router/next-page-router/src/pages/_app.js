24a061f4558b90874df57d7b58cf1a8b765bd053
yoorrll <lyl1567@naver.com>
2026-03-03 21:56:42 +0900

    feat: pre-fetching

--- 02-nextjs-page-router/next-page-router/src/pages/_app.js
+++ 02-nextjs-page-router/next-page-router/src/pages/_app.js
@@ -1,5 +1,34 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css.js';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.prefetch('/test');
+  }, [router]);
+
+  const handleNavigateToTest = () => {
+    router.push('/test');
+  };
+  return (
+    <>
+      <header>
+        <Link href="/">Home</Link>
+        &nbsp;
+        <Link href="/search/?q=김효효" prefetch={false}>
+          Search
+        </Link>
+        &nbsp;
+        <Link href="/movie/1">Movie</Link>
+        &nbsp;
+      </header>
+      <div>
+        <button onClick={handleNavigateToTest}>Go to Test</button>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
