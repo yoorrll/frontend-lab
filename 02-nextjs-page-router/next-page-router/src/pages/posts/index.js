f14b9cac319592094e8046bec65b2011a15e659d
yoorrll <lyl1567@naver.com>
2026-03-24 15:29:25 +0900

    feat: On-Demand ISR

---
+++ 02-nextjs-page-router/next-page-router/src/pages/posts/index.js
@@ -0,0 +1,10 @@
+// export default function Page() {
+//   const handleWrite = async () => {
+//     try {
+//       const post = await writePost();
+
+//       await fetch('http://localhost:3000/api/posts');
+//     } catch {}
+//   };
+//   return <button onClick={handleWrite}>글쓰기</button>;
+// }
